7095ab3697c21b6832828f73dd99440a52e35f9d
Adrián Vega <35713481+AdrianAVA9@users.noreply.github.com>
2018-10-22 17:45:32 -0600

    Get profile picture

---
+++ WebApi/WebApi/Controllers/PicturesController.cs
@@ -0,0 +1,79 @@
+﻿using CoreApi;
+using Exceptions;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Helper;
+
+namespace WebApi.Controllers
+{
+    public class PicturesController : SurveyOnlineController
+    {
+        private IProfileManager _profileManager { get; set; }
+
+        public PicturesController(IProfileManager profileManager)
+        {
+            _profileManager = profileManager;
+        }
+
+        [HttpGet]
+        [Route("api/profiles/{profileId}/pictures")]
+        public IHttpActionResult GetImages(Guid? profileId = null)
+        {
+            try
+            {
+                if (profileId == null) return BadRequest("Invalid profile Id");
+
+                if (!profileId.Equals(GetProfileId())) return Unauthorized();
+
+                var profile = _profileManager.GetProfile(new Entities_POJO.Profile { UserId = (Guid)profileId });
+
+                if (profile == null) return NotFound();
+
+                var result = new GetPictures().GetPicture(profile.ImagePath);
+
+                if (result.Status == CoreApi.ActionResult.ManagerActionStatus.Ok)
+                {
+                    var responseMessage = new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+                    {
+                        Content = new StreamContent(result.Entity)
+                    };
+                    responseMessage.Content.Headers.ContentType =
+                        new System.Net.Http.Headers.MediaTypeHeaderValue(
+                            string.Concat("image/", Path.GetExtension(result.Entity.Name).Substring(1)));
+
+                    return ResponseMessage(responseMessage);
+                }
+
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                ExceptionManager.GetInstance().Process(ex);
+                return InternalServerError();
+            }
+        }
+
+        //[HttpPost]
+        //[Route("api/profile/{profileI}/picture")]
+        //public IHttpActionResult PostPicture(Guid profileId)
+        //{
+        //    try
+        //    {
+        //        if (profileId == null) return BadRequest();
+
+        //        if (profileId.Equals(GetProfileId())) return Unauthorized();
+
+        //        return Ok();
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        ExceptionManager.GetInstance().Process(ex);
+        //        return InternalServerError();
+        //    }
+        //}
+
+    }
+}
---
+++ WebApi/WebApi/Helper/GetPictures.cs
@@ -0,0 +1,34 @@
+﻿using CoreApi.ActionResult;
+using System.Configuration;
+using System.IO;
+
+namespace WebApi.Helper
+{
+    public class GetPictures
+    {
+        private string SERVER_PATH_FOLDER { get; set; }
+
+        public GetPictures()
+        {
+            SERVER_PATH_FOLDER = ConfigurationManager.AppSettings["UPLOAD_DIR"];
+        }
+
+        public ManagerActionResult<FileStream> GetPicture(string imageId)
+        {
+            var route = System.Web.HttpContext.Current.Server.MapPath(SERVER_PATH_FOLDER);
+
+            var routeCompleted = string.Format("{0}/{1}", route, imageId);
+
+            if (!File.Exists(routeCompleted)) return new ManagerActionResult<FileStream>(null, ManagerActionStatus.NotFound);
+
+            var fileStream = File.OpenRead(routeCompleted);
+
+            return new ManagerActionResult<FileStream>(fileStream, ManagerActionStatus.Ok);
+        }
+
+        public ManagerActionResult<FileStream> DeletePicture(FileStream fileStream)
+        {
+            return new ManagerActionResult<FileStream>(null, ManagerActionStatus.Deleted);
+        }
+    }
+}
