d4df2f4082a274abda48a6fa535767029d7b7086
Adrián Vega <35713481+AdrianAVA9@users.noreply.github.com>
2018-10-22 18:58:03 -0600

    Register profile picture

---
+++ WebApi/WebApi/Helper/CreatePictures.cs
@@ -0,0 +1,36 @@
+﻿using CoreApi;
+using CoreApi.ActionResult;
+using Entities_POJO;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Web;
+
+namespace WebApi.Helper
+{
+    public class CreatePictures
+    {
+        private string SERVER_PATH_FOLDER { get; set; }
+        private IProfileManager _profileManger { get; set; }
+
+        public CreatePictures()
+        {
+            SERVER_PATH_FOLDER = ConfigurationManager.AppSettings["UPLOAD_DIR"];
+            _profileManger = new ProfileManager();
+        }
+
+        public ManagerActionResult<Profile> CreatePicture(Profile profile, HttpPostedFile fileStream)
+        {
+            var route = HttpContext.Current.Server.MapPath(SERVER_PATH_FOLDER);
+            var extension = Path.GetExtension(fileStream.FileName);
+            var pictureName = string.Format("{0}{1}{2}", Guid.NewGuid().ToString(), DateTime.Today.ToString("dd-MM-yyyy"), extension);
+            var routeCompleted = string.Concat(route, "/", pictureName);
+
+            fileStream.SaveAs(routeCompleted);
+
+            profile.ImagePath = pictureName;
+
+            return _profileManger.EditPicture(profile);
+        }
+    }
+}
